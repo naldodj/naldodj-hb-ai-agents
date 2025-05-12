c815263b1d152b653af0ef9517b9a8c83a3f3587
naldodj <marinaldo.jesus@gmail.com>
2025-05-11 22:46:09 -0300

    2025-05-11 22:45 UTC naldodj (go64.bat)

--- src/tst/go64.bat
+++ src/tst/go64.bat
@@ -2,16 +2,16 @@
 
     call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_curl_msvc.hbp -comp=msvc64
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_tip_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_curl_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_tip_msvc.hbp -comp=msvc64
 
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_msxml2_msvc.hbp -comp=msvc64
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_winhttp_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_msxml2_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_lmstudio_winhttp_msvc.hbp -comp=msvc64
 
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_curl_msvc.hbp -comp=msvc64
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_tip_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_curl_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_tip_msvc.hbp -comp=msvc64
 
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_msxml2_msvc.hbp -comp=msvc64
-    C:\Users\marin\Downloads\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_winhttp_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_msxml2_msvc.hbp -comp=msvc64
+    F:\harbour_msvc2022_64_20250216\bin\win\msvc64\hbmk2 hb_agents_ollama_winhttp_msvc.hbp -comp=msvc64
 
 @endlocal
